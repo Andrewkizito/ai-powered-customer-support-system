f116bb9ab6df846188ff4e0e89169a64265c38ee
andyclaudry <andrewkizito54@proton.me>
2026-06-10 11:07:41 +0300

    feat: initial setup

---
+++ src/frontend/frontend.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
+
+const elem = document.getElementById("root")!;
+(import.meta.hot.data.root ??= createRoot(elem)).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
